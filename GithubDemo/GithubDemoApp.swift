27eb2e9e68cd9bbc0fb8c96cc4dbf0bc51882c4a
turonueve <97420253+turonueve@users.noreply.github.com>
2022-08-23 10:11:41 -0700

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Avalokiteshvara on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
